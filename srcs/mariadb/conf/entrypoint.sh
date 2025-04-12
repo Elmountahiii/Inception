4ebe4639227a7b59c88e9f396c61447936cbcc0d
Youssef ElMountahi <yusuf.one@outlook.com>
2025-04-12 23:56:04 +0100

    adding wp

--- srcs/mariadb/conf/entrypoint.sh
+++ srcs/mariadb/conf/entrypoint.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -e
 
+# export DATABASE_NAME="wordpress"
+# export DATABASE_USER="wordpress_user"
+# export DATABASE_PASSWORD="superrandompassword"
+# export DATABASE_HOST="database"
+# export DOMAIN_NAME="yel-moun.42.fr"
+# export WORDPRESS_TITLE="El-Mountahi Website"
+# export ADMIN_USER="ElMountahi"
+# export ADMIN_PASSWORD="wp-pass11"
+# export ADMIN_EMAIL="yel-moun@student.1337.ma"
+# export USER_NAME="testuser"
+# export USER_EMAIL="testuser@example.com"
+# export USER_PASSWORD="userpass123"
+
 service mariadb start
 mariadb << EOF
 CREATE DATABASE IF NOT EXISTS \`${DATABASE_NAME}\`;
---
+++ srcs/wordpress/conf/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+
+mkdir -p /var/www/html/wordpress
+chown -R www-data:www-data /var/www/html/wordpress
+chmod -R 755 /var/www/html/wordpress
+
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar \
+    && chmod +x wp-cli.phar \
+    && mv wp-cli.phar /usr/local/bin/wp
+
+cd /var/www/html/wordpress
+
+wp core download --path=/var/www/html/wordpress --allow-root
+wp config create --path=/var/www/html/wordpress --dbname=$DATABASE_NAME --dbuser=$DATABASE_USER --dbpass=$DATABASE_PASSWORD --dbhost=mariadb:3306 --allow-root
+wp core install --url=$DOMAIN_NAME --title=$WORDPRESS_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --path=/var/www/html/wordpress --allow-root
+wp user create $USER_NAME $USER_EMAIL --role=author --user_pass=$USER_PASSWORD --path=/var/www/html/wordpress --allow-root
+wp config set WP_CACHE true --raw --type=constant --allow-root
+
+
+exec php-fpm8.2 -F

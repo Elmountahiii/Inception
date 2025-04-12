7513fb9255b0ca2905459e44ee8602b6aa038722
Youssef El Mountahi <yel-moun@c2r5p10.1337.ma>
2025-04-12 20:30:04 +0100

    adding mariadb

---
+++ srcs/mariadb/conf/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+service mariadb start
+mariadb << EOF
+CREATE DATABASE IF NOT EXISTS \`${DATABASE_NAME}\`;
+CREATE USER IF NOT EXISTS \`${DATABASE_USER}\`@'%' IDENTIFIED BY '${DATABASE_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${DATABASE_NAME}\`.* TO \`${DATABASE_USER}\`@'%';
+FLUSH PRIVILEGES;
+EOF
+mariadb  -e "SHOW DATABASES;"
+service mariadb stop
+
+exec mariadbd-safe

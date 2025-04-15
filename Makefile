503155c805c3c0bc063ee19419184e3560063309
Youssef ElMountahi <yusuf.one@outlook.com>
2025-04-15 17:41:00 +0100

    all working but not done

--- Makefile
+++ Makefile
@@ -1,15 +1,9 @@
 all:
 	cd srcs && docker-compose up -d
 
-machineSetup:
-	sudo apt-get update
-	sudo apt-get install ca-certificates curl
-	sudo install -m 0755 -d /etc/apt/keyrings
-	sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-	sudo chmod a+r /etc/apt/keyrings/docker.asc
-	echo \
-  	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  	$(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  	sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-	sudo apt-get update
-	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+clean:
+	@if [ -n "$$(docker ps -qa)" ]; then docker stop $$(docker ps -qa); fi
+	@if [ -n "$$(docker ps -qa)" ]; then docker rm $$(docker ps -qa); fi
+	@if [ -n "$$(docker images -qa)" ]; then docker rmi -f $$(docker images -qa); fi
+	@if [ -n "$$(docker volume ls -q)" ]; then docker volume rm $$(docker volume ls -q); fi
+	@if [ -n "$$(docker network ls -q | grep -v 'bridge\|host\|none')" ]; then docker network rm $$(docker network ls -q | grep -v 'bridge\|host\|none'); fi
--- srcs/requirements/mariadb/conf/entrypoint.sh
+++ srcs/requirements/mariadb/conf/entrypoint.sh
@@ -1,33 +1,26 @@
-#!/bin/bash
-
-# If MariaDB is already running (mysqld.pid exists), skip everything.
-# If not:
-#     Update MariaDB configuration (50-server.cnf).
-#     Check if the database exists.
-#     Start MariaDB.
-#     Create the database and users.
-#     Set passwords and privileges.
-#     Stop MariaDB (to ensure changes apply).
-# Finally, start MariaDB in safe mode for normal operation.
-
-export DATABASE_ROOT_PASSWORD=hello
-
-sed -i 's/= 127.0.0.1/= 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
-sed -i 's/basedir/port\t\t\t\t\t= 3306\nbasedir/' /etc/mysql/mariadb.conf.d/50-server.cnf
-echo "Inception : 50-server.cnf updated."
-
-if [ ! -d "/var/lib/mysql/${DATABASE_NAME}" ];
-then
-	echo "Inception : ${DATABASE_NAME} database is being created."
-	mysqld_safe &
-	sleep 2
-	mysql -e "CREATE DATABASE IF NOT EXISTS ${DATABASE_NAME};"
-	mysql -e "CREATE USER IF NOT EXISTS '${DATABASE_USER}'@'%' IDENTIFIED BY '${DATABASE_PASSWORD}';"
-	mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${DATABASE_USER}'@'%' IDENTIFIED BY '${DATABASE_PASSWORD}';"
-	mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${DATABASE_ROOT_PASSWORD}';"
-	mysql -e "FLUSH PRIVILEGES;"
-	mysql -u root --skip-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DATABASE_ROOT_PASSWORD}';"
-	mysqladmin -u root -p$DATABASE_ROOT_PASSWORD shutdown
+#!/bin/sh
+set -e
+echo "Initializing database..."
+if [ ! -d "/var/lib/mysql/${DATABASE_NAME}" ]; then
+echo "Creating ${DATABASE_NAME} database and ${DATABASE_USER} user..."
+    cat << EOF > /tmp/create_db.sql
+USE mysql;
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DATABASE_ROOT_PASSWORD}';
+CREATE DATABASE IF NOT EXISTS \`${DATABASE_NAME}\`;
+CREATE USER IF NOT EXISTS '${DATABASE_USER}'@'%' IDENTIFIED BY '${DATABASE_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${DATABASE_NAME}\`.* TO '${DATABASE_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
+if mariadbd --user=mysql --bootstrap < /tmp/create_db.sql; then
+echo "Database and user created successfully."
+else
+echo "Error: Failed to initialize database." >&2
+exit 1
+fi
+rm -f /tmp/create_db.sql
+else
+    echo "Database '${DATABASE_NAME}' already exists, skipping initialization."
 fi
 
-exec "mysqld_safe"
+exec $@
